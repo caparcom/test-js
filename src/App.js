f493d3512064a10463b8f7a614b814b1ce348e25
caparcom <caleb_combs@yahoo.com>
2023-12-08 14:42:31 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './App.css';
+
+import App from './App';
+
+export default function Board() {
+  return (
+  <>
+  <div className="board-row1">
+    <button className="square">1</button>
+    <button className="square">2</button>
+    <button className="square">3</button>
+  </div>
+ <br />
+  <div className="board-row2">
+    <button className="square">4</button>
+    <button className="square">5</button>
+    <button className="square">6</button>
+  </div>
+  <br />
+  
+  <div className="board-row3">
+    <button className="square">7</button>
+    <button className="square">8</button>
+    <button className="square">9</button>
+  </div>
+
+  </>
+  );
+}
